db8883ad703e81833a2e0f62fd0b154595ea8b94
Laura-Oliveira <laura.regina.cp@gmail.com>
2025-09-10 17:57:14 -0300

    [FrontEnd] Implementation of design elements to improve the layout

--- app/src/main/java/com/weather/compose/view/WeatherCard.kt
+++ app/src/main/java/com/weather/compose/view/WeatherCard.kt
@@ -1,37 +1,38 @@
 package com.weather.compose.view
 
-
 import com.weather.compose.R
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.AsyncImage
-import com.weather.compose.model.CountryCode
 import com.weather.compose.viewModel.WeatherViewModel
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun WeatherCard(weatherViewModel: WeatherViewModel = viewModel())
@@ -48,54 +49,89 @@ fun WeatherCard(weatherViewModel: WeatherViewModel = viewModel())
     }
 
     Surface {
-        //Column Global
-        Column(modifier = Modifier.fillMaxSize()) {
-            //Title
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(10.dp),
-                horizontalArrangement = Arrangement.Center
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = colorResource(R.color.background))
             ) {
-                Text(
-                    text = "$title",
-                    color = Color.Blue
-                )
 
-                Spacer(modifier = Modifier.width(30.dp)) // espaço de 16dp
-            }//Row - Title
-
-
-            LazyColumn(modifier = Modifier.padding(16.dp)) {
-                items(cityWeather) { cityWeather ->
-                    Row(
+            //Column Global
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+//                    .padding(top = 30.dp)
+                    .padding(30.dp)
+                    .clip(RoundedCornerShape(30.dp))
+                    //.border(width = 1.dp, Color.White, shape = RoundedCornerShape(50.dp))
+                    .background(color = colorResource(R.color.square))
+            ) {
+                //Title
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 20.dp)
+                        .padding(10.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.cat_weather),
+                        contentDescription = "Icon App",
+                        contentScale = ContentScale.Crop,
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 8.dp),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        // Row Into City: Flag + Name + Temperature
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Start
-                        ) {
-                            //Flag (Composable)
-                            CountryFlag(cityWeather.countryCode.country)
+                            .size(50.dp)
+                            .clip(RoundedCornerShape(50.dp))
+                    )
 
-                            //Column: City Name + Temperature
-                            Column(
-                                modifier = Modifier.padding(start = 10.dp)
+                    Spacer(modifier = Modifier.padding(5.dp))
+
+                    Text(
+                        text = "$title",
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp
+                    )
+
+                    Spacer(modifier = Modifier.width(30.dp)) // espaço de 16dp
+                }//Row - Title
+
+
+                LazyColumn(modifier = Modifier.padding(16.dp)) {
+                    items(cityWeather) { cityWeather ->
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 8.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            // Row Into City: Flag + Name + Temperature
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Start
                             ) {
-                                Text(text = cityWeather.city, color = Color.Blue)
-                                Text(
-                                    text = "${cityWeather.main.temperature.toInt()} ºC",
-                                    color = Color.DarkGray
-                                )
-                            }//Column: City Name + Temperature
-                        }//Row Into City: Flag + Name + Temperature
-                    }//Row List Cities
-                }//City Itens
-            }//LazyColumn
-        }//Column Global
+                                //Flag (Composable)
+                                CountryFlag(cityWeather.countryCode.country)
+
+                                //Column: City Name + Temperature
+                                Column(
+                                    modifier = Modifier.padding(start = 10.dp)
+                                ) {
+                                    Text(
+                                        text = cityWeather.city,
+                                        color = Color.White,
+                                        fontSize = 16.sp
+                                    )
+                                    Text(
+                                        text = "${cityWeather.main.temperature.toInt()} ºC",
+                                        color = Color.White,
+                                        fontSize = 16.sp
+                                    )
+                                }//Column: City Name + Temperature
+                            }//Row Into City: Flag + Name + Temperature
+                        }//Row List Cities
+                    }//City Itens
+                }//LazyColumn
+            }//Column Global
+        }
     }//Surface
 }
